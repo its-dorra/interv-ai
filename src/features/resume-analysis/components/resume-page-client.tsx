ce70697a9f57c4b15ea0c3ab8663c36ca2390264
Islam Mohamed Eldera Abderrezak <islam.abderrezak@ensia.edu.dz>
2025-10-17 23:43:30 +0100

    feat: enhance resume analysis results with detailed feedback display and loading states

--- src/features/resume-analysis/components/resume-page-client.tsx
+++ src/features/resume-analysis/components/resume-page-client.tsx
@@ -18,6 +18,7 @@ import type { DeepPartial } from "ai";
 import { Skeleton } from "@/components/ui/skeleton";
 import {
   Accordion,
+  AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
@@ -144,12 +145,13 @@ export default function ResumePageClient({ jobInfoId }: { jobInfoId: string }) {
 }
 
 type Keys = Exclude<keyof z.infer<typeof AiAnalyzeSchema>, "overallScore">;
+type AiAnalysis = z.infer<typeof AiAnalyzeSchema>;
 
 function AnalysisResults({
   aiAnalysis,
   isLoading,
 }: {
-  aiAnalysis: DeepPartial<z.infer<typeof AiAnalyzeSchema>> | undefined;
+  aiAnalysis: DeepPartial<AiAnalysis> | undefined;
   isLoading: boolean;
 }) {
   if (!isLoading && !aiAnalysis) return null;
@@ -187,6 +189,35 @@ function AnalysisResults({
                     score={category?.score}
                   />
                 </AccordionTrigger>
+                <AccordionContent>
+                  <div className="space-y-4">
+                    <div className="text-muted-foreground">
+                      {!category?.summary ? (
+                        <span>
+                          <Skeleton />
+                          <Skeleton className="w-3/4" />
+                        </span>
+                      ) : (
+                        category.summary
+                      )}
+                    </div>
+                    <div className="space-y-3">
+                      {!category?.feedback ? (
+                        <>
+                          <Skeleton className="h-16" />
+                          <Skeleton className="h-16" />
+                          <Skeleton className="h-16" />
+                        </>
+                      ) : (
+                        category.feedback.map((item, index) => {
+                          if (!item) return null;
+
+                          return <FeedbackItem item={item} />;
+                        })
+                      )}
+                    </div>
+                  </div>
+                </AccordionContent>
               </AccordionItem>
             );
           })}
@@ -218,5 +249,35 @@ function CategoryAccordionHeader({
       </div>
       {score == null ? <Skeleton className="w-12" /> : `${score}/10`}
     </div>
+  );
+}
+
+function FeedbackItem({
+  item: { message, name, type },
+}: {
+  item: Partial<AiAnalysis["ats"]["feedback"][number]>;
+}) {
+  if (!name || !message || !type) return null;
+
+  const getColors = () => {
+    switch (type) {
+      case "strength":
+        return "bg-primary/10 border border-primary/50";
+      case "major-improvement":
+        return "bg-destructive/10 dark:bg-destructive/2- border border-destructive/50 dark:border-destructive/70";
+      case "minor-improvement":
+        return "bg-warning/10 border border-warning/40";
+      default:
+        throw new Error(`Unknown feedback type : ${type satisfies never}`);
+    }
+  };
+
+  return (
+    <div
+      className={cn(
+        "flex items-baseline gap-3 pl-3 pr-5 py-5 rounded-lg",
+        getColors()
+      )}
+    ></div>
   );
 }
