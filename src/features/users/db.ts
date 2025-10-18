2719e5db5da37498e85fd89318a208d5bcd7e849
Islam Mohamed Eldera Abderrezak <islam.abderrezak@ensia.edu.dz>
2025-10-18 14:46:03 +0100

    refactor: ensure database operations await completion and revalidate user cache after upsert and delete actions

--- src/features/users/db.ts
+++ src/features/users/db.ts
@@ -1,17 +1,22 @@
 import db from "@/drizzle/db";
 import { type InsertUser, UserTable } from "@/drizzle/schema";
 import { eq } from "drizzle-orm";
+import { revalidateUserCache } from "./db-cache";
 
 export async function upsertUser(user: InsertUser) {
-  return db
+  await db
     .insert(UserTable)
     .values(user)
     .onConflictDoUpdate({
       target: [UserTable.id],
       set: user,
     });
+
+  revalidateUserCache(user.id);
 }
 
 export async function deleteUser(userId: string) {
-  return db.delete(UserTable).where(eq(UserTable.id, userId));
+  await db.delete(UserTable).where(eq(UserTable.id, userId));
+
+  revalidateUserCache(userId);
 }
